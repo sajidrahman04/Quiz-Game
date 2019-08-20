c254083d1a46df2be1c004ccb0b4056fa033fd44
Sajid Rahman <potatorahman@gmail.com>
2019-08-20 00:13:56 -0400

    Added start button which hides table until game ends

--- src/App.js
+++ src/App.js
@@ -14,38 +14,61 @@ class App extends React.Component {
       foundItems: [false, false, false, false],
       numItems: 4,
       numCols: 2,
-      currScore: 0
+      currScore: 0,
+      gameStart: false
     }
     this.checkTerm = this.checkTerm.bind(this);
+    this.gameStart = this.gameStart.bind(this);
   }
 
   checkTerm(term){
     var foundAnswer = false;
+    var newScore = this.state.currScore;
     for (var i = 0; i < this.state.numItems; i++) {
       var newAnswers = this.state.foundItems;
       if(this.state.foundItems[i] === false && term.toLowerCase() === this.state.allAnswers[i].toLowerCase()){
         newAnswers[i] = true;
         foundAnswer = true;
+        newScore += 1;
         this.setState({
-          currScore: this.state.currScore+1
+          currScore: newScore
         });
       }
     }
     this.setState({
       foundItems: newAnswers
     });
+    if(newScore === this.state.numItems){
+      this.setState({
+        gameStart: false
+      })
+    }
     return foundAnswer;
+  }
+
+  gameStart(){
+    this.setState({
+      foundItems: [false, false, false, false],
+      gameStart: true
+    })
   }
 
   render(){
     return (
       <div className="App">
-        <AnswerTable numItems = {this.state.numItems} 
-          foundItems = {this.state.foundItems} answers = {this.state.allAnswers}
-          numCols = {this.state.numCols}>
-        </AnswerTable>
-        <AnswerInput checkTerm = {this.checkTerm}></AnswerInput>
-        <ScoreBoard currScore={this.state.currScore} total={this.state.numItems}></ScoreBoard>
+        
+        {this.state.gameStart ? 
+          <div>
+          <AnswerTable numItems = {this.state.numItems} 
+            foundItems = {this.state.foundItems} answers = {this.state.allAnswers}
+            numCols = {this.state.numCols}>
+          </AnswerTable>
+          <AnswerInput checkTerm = {this.checkTerm}></AnswerInput>
+          <ScoreBoard currScore={this.state.currScore} total={this.state.numItems}></ScoreBoard>
+        </div>
+        :<button onClick={this.gameStart}>START GAME</button>}
+        
+        
       </div>
     );
   }
